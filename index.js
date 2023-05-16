f156df17a4f85f30254ea09ce3344f752fd9a947
ChannanagoudaM <129206262+ChannanagoudaM@users.noreply.github.com>
2023-05-16 17:00:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let pictures=document.getElementById('pictures');
+pictures.addEventListener('click',showPictures);
+function showPictures()
+{
+   window.location.href="https://unsplash.com/s/photos/hotel";
+}
+let about=document.getElementById('about');
+about.addEventListener('click',aboutHotel);
+function aboutHotel()
+{
+    window.location.href="https://en.wikipedia.org/wiki/Taj_Hotels";
+}
---
+++ menu.js
@@ -0,0 +1,6 @@
+let button=document.getElementById('button');
+button.addEventListener('click',showMsg)
+function showMsg()
+{
+    alert("Witer will be here within 5mins");
+}
